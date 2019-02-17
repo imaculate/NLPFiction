b3ae04a9be46fd4627c718575d35bbd04ada2fd7
Imaculate Mosha <imaculatemosha@yahoo.com>
2019-02-16 20:39:49 -0800

    clean up wikipedia scraper

--- wikipedia-scraper.py
+++ wikipedia-scraper.py
@@ -3,6 +3,7 @@ import urllib
 import urllib.request
 import re
 import os
+import argparse
 
 anchor_pattern = "(\[\[){1}[a-zA-Z0-9_ ]*(\]\]){1}"
 
@@ -27,44 +28,49 @@ def scrape_wiki_and_save(title, save_dir):
         f.write(wiki_text)
     return wiki_text
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--save_dir', type=str, default='data\\benfranklin',
+                       help='directory to store scraped articles')
+    parser.add_argument('--title', type=str, default='Benjamin Franklin',
+                help='topic to begin scraping on')
+    parser.add_argument('--combine_file', type=str, default='input.txt',
+                        help='filename where articles will be written to after cleaning up')
+    args = parser.parse_args()
+    scrape_wikis(args)
+    clean_data(args)
 
-title = "Benjamin Franklin"
-save_dir = "C:\\Users\\imacu\\Documents\\word-rnn-tensorflow\\data\\benfranklin"
-'''
-wiki_text = scrape_wiki_and_save(title, save_dir)
-anchors = []
-[anchors.append(m.group().strip('[').strip(']')) for m in re.finditer(anchor_pattern, wiki_text)]
+def scrape_wikis(args):
+    wiki_text = scrape_wiki_and_save(args.title, args.save_dir)
+    anchors = []
+    [anchors.append(m.group().strip('[').strip(']')) for m in re.finditer(anchor_pattern, wiki_text)]
 
-
-for anchor in anchors:
-    print("Scraping "+ anchor)
-    scrape_wiki_and_save(anchor, save_dir)
-'''
-
-
-# clean up data
-input_text = ""
-compined_file = "input2.txt"
-fw = open(os.path.join(save_dir, compined_file), "w+", encoding="utf-8")
-for root, dirs, files in os.walk(save_dir):
-    for filename in files:
-        if filename.startswith("wiki_"):
-            with open(os.path.join(save_dir, filename), "r", encoding="utf-8") as f:
-                input_text = f.read()
-
-            # remove everything enclosed by {{ }}, links and beginning text that is not really needed
-            input_text = re.sub("(\{\{){1}.*(\}\}){1}", "", input_text)
-            # remove the stuff in between, get all the way to references
-            try:
-                input_text = input_text[:input_text.index("==References==")]
-                print("References section found!")
-            except ValueError:
-                print("Substring ==References== not found")
-
-            # remove [[]] for texts with just letters and numbers
-            input_text = re.sub(r"[^a-z0-9\.,]+", " ", input_text.lower())
-
-            fw.write(input_text)
+    for anchor in anchors:
+        print("Scraping "+ anchor)
+        scrape_wiki_and_save(anchor, args.save_dir)
 
 
-fw.close()
+def clean_data(args):
+    fw = open(os.path.join(args.save_dir, args.combine_file), "w+", encoding="utf-8")
+    for root, dirs, files in os.walk(args.save_dir):
+        for filename in files:
+            if filename.startswith("wiki_"):
+                with open(os.path.join(args.save_dir, filename), "r", encoding="utf-8") as f:
+                    input_text = f.read()
+
+                # remove everything enclosed by {{ }}, links and beginning text that is not really needed
+                input_text = re.sub("(\{\{){1}.*(\}\}){1}", "", input_text)
+                # remove the stuff in between, get all the way to references
+                try:
+                    input_text = input_text[:input_text.index("==References==")]
+                    print("References section found!")
+                except ValueError:
+                    print("Substring ==References== not found")
+
+                # remove [[]] for texts with just letters and numbers
+                input_text = re.sub(r"[^a-z0-9\.,]+", " ", input_text.lower())
+
+                fw.write(input_text)
+
+
+    fw.close()
